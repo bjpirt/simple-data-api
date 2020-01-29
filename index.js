35f6e72333c480e4543786a10f29c86f7937e0b4
Ben Pirt <ben@pirt.co.uk>
2020-01-29 21:58:59 +0000

    Extract error handling into utility function

--- index.js
+++ index.js
@@ -14,81 +14,54 @@ const send = (data) => {
   };
 }
 
-module.exports.listGroups = async () => {
-  try {
-    const groups = await useCases.listGroups();
-    return send(groups);
-  } catch (err) {
-    console.log(err);
-    return { statusCode: 500 }
-  }
-}
-
-module.exports.createGroup = async (event) => {
-  try {
-    const group = await useCases.createGroup(JSON.parse(event.body));
-    return {
-      statusCode: 302,
-      headers: { Location: `/groups/${group.id}` }
-    };
-  } catch (err) {
-    console.log(err);
-    return { statusCode: 500 }
-  }
-}
-
-module.exports.getGroup = async (event) => {
-  try {
-    const group = await useCases.getGroup(event.pathParameters.groupId);
-    if(group){
-      return send(group);
-    }else{
-      return { statusCode: 404 };
+const catchError = (fn) => {
+  return (async (event) => {
+    try {
+      return await fn(event)
+    } catch (err) {
+      if(err === 'NotFoundException'){
+        return { statusCode: 404 };
+      }else{
+        console.log(err);
+        return { statusCode: 500 }
+      }
     }
-  } catch (err) {
-    console.log(err);
-    return { statusCode: 500 }
-  }
+  })
 }
 
-module.exports.updateGroup = async (event) => {
-  try {
-    await useCases.updateGroup(event.pathParameters.groupId, JSON.parse(event.body));
-    return { statusCode: 204 };
-  } catch (err) {
-    if(err === 'NotFoundException'){
-      return { statusCode: 404 };
-    }else{
-      console.log(err);
-      return { statusCode: 500 }
-    }
-  }
-}
+module.exports.listGroups = catchError(async (event) => {
+  const groups = await useCases.listGroups();
+  return send(groups);
+});
 
-module.exports.createMetrics = async (event) => {
-  try {
-    await useCases.createBulkReadings(event.pathParameters.groupId, JSON.parse(event.body));
-    return { statusCode: 204 };
-  } catch (err) {
-    if(err === 'NotFoundException'){
-      return { statusCode: 404 };
-    }else{
-      console.log(err);
-      return { statusCode: 500 }
-    }
-  }
-}
+module.exports.createGroup = catchError(async (event) => {
+  const group = await useCases.createGroup(JSON.parse(event.body));
+  return {
+    statusCode: 302,
+    headers: { Location: `/groups/${group.id}` }
+  };
+});
 
-module.exports.getMetric = async (event) => {
-  try {
-    const values = await useCases.getMetric(event.pathParameters.groupId, event.pathParameters.metricId, event.queryStringParameters);
-    return send(values);
-  } catch (err) {
-    if(err === 'NotFoundException'){
-      return { statusCode: 404 };
-    }else{
-      console.log(err);
-      return { statusCode: 500 }
-    }
+module.exports.getGroup = catchError(async (event) => {
+  const group = await useCases.getGroup(event.pathParameters.groupId);
+  if(group){
+    return send(group);
+  }else{
+    return { statusCode: 404 };
   }
-}
+});
+
+module.exports.updateGroup = catchError(async (event) => {
+  await useCases.updateGroup(event.pathParameters.groupId, JSON.parse(event.body));
+  return { statusCode: 204 };
+});
+
+module.exports.createMetrics = catchError(async (event) => {
+  await useCases.createBulkReadings(event.pathParameters.groupId, JSON.parse(event.body));
+  return { statusCode: 204 };
+});
+
+module.exports.getMetric = catchError(async (event) => {
+  const values = await useCases.getMetric(event.pathParameters.groupId, event.pathParameters.metricId, event.queryStringParameters);
+  return send(values);
+});
